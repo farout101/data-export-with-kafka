3de3e84e5c75e5d93f0757c750067e18bc648a2f
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-29 10:14:53 +0630

    updated

--- request-producer/src/main/java/com/test/requestproducer/controller/RequestHandler.java
+++ request-producer/src/main/java/com/test/requestproducer/controller/RequestHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/api/v1/request")
@@ -24,16 +23,6 @@ public class RequestHandler {
     @PostMapping("")
     public void handleRequest(@RequestBody RequestRecord request) {
         producerService.sendRequest(request);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
-        log.info(
-                "Request received: export_id={}, file_format={}, transaction_type={}, start_datetime={}, end_datetime={}",
-                request.export_id(),
-                request.file_format(),
-                request.transaction_type(),
-                request.start_datetime().format(formatter),
-                request.end_datetime().format(formatter)
-        );
+        log.info("Controller Called with RequestID : {}", request);
     }
 }
