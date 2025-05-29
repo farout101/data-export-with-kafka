cce277f5bc72f5e392e7d565bd5f9c13f720effe
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-29 10:32:05 +0630

    compose files

--- request-consumer/src/main/java/com/dataexport/consumer/config/ProjectConfig.java
+++ request-consumer/src/main/java/com/dataexport/consumer/config/ProjectConfig.java
@@ -1,12 +1,12 @@
-package com.dataexport.consumer.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ProjectConfig {
-
-    @Bean
-
-
-}
+//package com.dataexport.consumer.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ProjectConfig {
+//
+//    @Bean
+//
+//
+//}
